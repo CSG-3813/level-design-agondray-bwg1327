26b29673b068e1c300ea2f3f90af691d82a8ec16
bwg1327 <bwg1327@utulsa.edu>
2022-12-14 13:06:27 -0600

    kubh

---
+++ UnityAgonDray/Assets/custom/scripts/UIhud.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class UIhud : MonoBehaviour
+{
+    int keys;
+    int maxKeys = 3;
+
+    public TMP_Text keysDisplay;
+    
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        //gm = GameManager.GM;
+        //need to make GM
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //update the keys display
+        //keys = gm.keys;
+        //if (keysDisplay) { keysDisplay.text = "Keys: " + keys + " /" + maxKeys; }
+        //GetGameStats();
+        //SetHUD();
+    }
+    
+
+
+}
